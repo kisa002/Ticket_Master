78d4886d0b0e3011511702a29468fd825daf17a8
kisa002 <vnycall74@naver.com>
2017-10-28 01:47:40 +0900

    타이틀 변경 및 하단 구현중

---
+++ Assets/01.Script/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+
+
+    public int hp;
+    public GameObject hpDownEffect;
+    public Text hpDownNumLabel;
+
+    public void Damage(int num)
+    {
+        hp = hp - num;
+        hpDownNumLabel.text = num.ToString();
+        hpDownEffect.SetActive(true);
+    }
+
+
+}
---
+++ Assets/01.Script/ScratchManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScratchManager : MonoBehaviour {
+
+    public GameObject imageHidden;
+
+    int width = 500;
+    int height = 300;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartHidden()
+    {
+        for(int i=0; i<width; i+= 10)
+            for(int j=0; j<height; j+= 10)
+                Instantiate(imageHidden, new Vector2(transform.position.x + i, transform.position.y + j), Quaternion.identity);
+    }
+}
